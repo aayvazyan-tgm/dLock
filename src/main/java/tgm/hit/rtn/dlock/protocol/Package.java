c6f2f5474d45ac9252fbcbfe2b1f11e938377150
aayvazyan-tgm <ariay@live.at>
2014-10-13 00:09:14 +0200

    we do not need the sender information, never rely on a client

--- src/main/java/tgm/hit/rtn/dlock/protocol/Package.java
+++ src/main/java/tgm/hit/rtn/dlock/protocol/Package.java
@@ -1,15 +1,11 @@
 package tgm.hit.rtn.dlock.protocol;
 
 import tgm.hit.rtn.dlock.Peer;
-
 import java.io.Serializable;
 
 public class Package  implements Serializable {
 
 	public PackageType type;
-
-	public Peer sender;
-
 	public String msg;
 
 }
