6e3b922127668bbf96f4d16fd573be2513d7cefb
aayvazyan-tgm <ariay@live.at>
2014-10-13 01:26:08 +0200

    initial commit

--- src/main/java/tgm/hit/rtn/dlock/RequestHandlers/UnlockRequestHandler.java
+++ src/main/java/tgm/hit/rtn/dlock/RequestHandlers/UnlockRequestHandler.java
@@ -4,21 +4,21 @@ import tgm.hit.rtn.dlock.Peer;
 import tgm.hit.rtn.dlock.PeerManager;
 import tgm.hit.rtn.dlock.UDPLockServer.ThreadedConnection;
 import tgm.hit.rtn.dlock.protocol.requests.Bye;
-import tgm.hit.rtn.dlock.protocol.requests.Lock;
 import tgm.hit.rtn.dlock.protocol.requests.Request;
+import tgm.hit.rtn.dlock.protocol.requests.Unlock;
 
 /**
  * @author Ari Michael Ayvazyan
  * @version 13.10.2014
  */
-public class LockRequestHandler implements RequestListener {
-    public final static LockRequestHandler INSTANCE = new LockRequestHandler();
-    private LockRequestHandler(){};
+public class UnlockRequestHandler implements RequestListener {
+    public final static UnlockRequestHandler INSTANCE = new UnlockRequestHandler();
+    private UnlockRequestHandler(){};
     @Override
     public void handleRequest(Request request, ThreadedConnection threadedConnection) {
-        if(request instanceof Lock){
-            Lock byeReq=(Lock)request;
-            //TODO return locked/not locked status!
+        if(request instanceof Unlock){
+            Unlock byeReq=(Unlock)request;
+            //Because of a "Is the system locked?" request. unlock requests do not need to be handled
         }
     }
 }
