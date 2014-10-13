1803b4e7364ea513305f56046938b9dd6c4a1b70
Ari Ayvazyan <ariay@live.at>
2014-10-13 14:37:38 +0200

    added some javadoc

--- src/main/java/tgm/hit/rtn/dlock/DLock.java
+++ src/main/java/tgm/hit/rtn/dlock/DLock.java
@@ -1,71 +1,96 @@
 package tgm.hit.rtn.dlock;
 
 import java.util.concurrent.TimeUnit;
-
+/**
+ * @author Ari Michael Ayvazyan
+ * @version 13.10.2014
+ */
 public class DLock implements GotLock {
 
-	private PeerManager peerManager;
+    private PeerManager peerManager;
 
-	private DLockClient dLockClient;
+    private DLockClient dLockClient;
 
-	public DLock(PeerManager peerManager, DLockClient dLockClient) {
+    public DLock(PeerManager peerManager, DLockClient dLockClient) {
         this.peerManager = peerManager;
         this.dLockClient = dLockClient;
 
     }
 
-	public void lockWhile(Callback call, Object... params) {
+    /**
+     * Locks while the call is being executed. (blocking)
+     * Waits until The System is not locked.
+     *
+     * @param call
+     * @param params
+     */
+    public void lockWhile(Callback call, Object... params) {
         lockRequest();
         call.run(params);
         unlock();
-	}
+    }
 
     /**
      * Acquires the lock.
      * If the lock is not available then the current thread becomes disabled for thread scheduling purposes and lies dormant until the lock has been acquired.
      */
-	public void lock() throws InterruptedException {
-        while(!lockRequest()){
+    public void lock() throws InterruptedException {
+        while (!lockRequest()) {
             Thread.sleep(Settings.RETRY_TIME_NANO);
         }
-	}
+    }
 
     /**
      * Acquires the lock only if it is free at the time of invocation.
-
-     Acquires the lock if it is available and returns immediately with the value true. If the lock is not available then this method will return immediately with the value false.
+     * <p/>
+     * Acquires the lock if it is available and returns immediately with the value true. If the lock is not available then this method will return immediately with the value false.
+     *
      * @return Tries to Acquires the lock if it is available and returns immediately with the value true.
      */
-	public boolean tryLock() {
-		return lockRequest();
-	}
+    public boolean tryLock() {
+        return lockRequest();
+    }
 
     private boolean lockRequest() {
         return false;
     }
 
     /**
+     * Acquires the lock if it is free within the given waiting time and the current thread has not been interrupted.
+     * <p/>
+     * If the lock is available this method returns immediately with the value true. If the lock is not available then the current thread becomes disabled for thread scheduling purposes and lies dormant until one of three things happens:
+     * <p/>
+     * The lock is acquired by the current thread; or
+     * Some other thread interrupts the current thread, and interruption of lock acquisition is supported; or
+     * The specified waiting time elapses
+     * <p/>
+     * If the lock is acquired then the value true is returned.
+     * <p/>
+     * If the specified waiting time elapses then the value false is returned. If the time is less than or equal to zero, the method will not wait at all.
      *
-     * @param time
-     * @param unit
-     * @return
-     * @throws InterruptedException
+     * @param time - the maximum time to wait for the lock
+     * @param unit - the time unit of the time argument
+     * @return true if the lock was acquired and false if the waiting time elapsed before the lock was acquired
+     * @throws InterruptedException if the current thread is interrupted while acquiring the lock (and interruption of lock acquisition is supported)
      */
-	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
         dLockClient.lock();
         Thread.sleep(unit.toMillis(time));
         return false;
-	}
+    }
 
-	public void unlock() {
+    /**
+     * Releases the lock.
+     */
+    public void unlock() {
         dLockClient.unlock();
-	}
+    }
 
 
-	/**
-	 * @see tgm.hit.rtn.dlock.GotLock#gotLock()
-	 */
-	public void gotLock() {
+    /**
+     * @see tgm.hit.rtn.dlock.GotLock#gotLock()
+     */
+    public void gotLock() {
 
     }
 }
